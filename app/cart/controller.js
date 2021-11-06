7cc6f923745f038a092898576856df5ba454c726
Andre William <andrecool.cool@gmail.com>
2021-11-06 16:12:13 +0700

    finished

--- app/cart/controller.js
+++ app/cart/controller.js
@@ -19,16 +19,16 @@ async function update(req, res, next){
         const {items} = req.body;
 
         //ekstrak _id dari masing" item
-        const productIds = items.map(itm => itm._id); 
+        const productIds = items.map(itm => itm.product._id); 
 
         //cari data _id pada produk
         const products = await Product.find({_id: {$in: productIds}});
 
         let cartItems = items.map(item => {
-            let relatedProduct = products.find(product => product._id.toString() === item._id);
+            let relatedProduct = products.find(product => product._id.toString() === item.product._id);
 
             return {
-                _id: relatedProduct._id,
+                
                 product: relatedProduct._id,
                 price: relatedProduct.price,
                 image_url: relatedProduct.image_url,
